c68f141f9186b7a6e32e1a0a4e5e082282b41823
aymanfarsi <aymanfarsi99@gmail.com>
2024-05-21 22:46:36 -0400

    Working on Linux

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,12 @@ use tokio::runtime::Runtime;
 use stash::utils::{run_first_error_app::run_first_error_app, run_main_app::run_main_app};
 
 fn check_env() -> Result<(), String> {
-    #[cfg(target_os = "windows")]
+    #[cfg(any(target_os = "windows", target_os = "linux"))]
     {
         Ok(())
     }
 
-    #[cfg(not(target_os = "windows"))]
+    #[cfg(target_os = "macos")]
     {
         Err("Stash is only supported on Windows for now!".to_string())
     }
